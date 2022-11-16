5522650f7f6fba6c1afecca987283ec8b1e64900
Allister Driscoll <allisterdrisc@gmail.com>
2022-11-15 20:07:01 -0500

    Add solutions to problems in files

---
+++ the_basics/exercise_3.rb
@@ -0,0 +1,13 @@
+movies = {
+  grey_gardens: 1975,
+  the_grudge: 2004,
+  warm_bodies: 2013,
+  donnie_darko: 2001,
+  halloween: 1981
+}
+
+puts movies[:grey_gardens]
+puts movies[:the_grudge]
+puts movies[:warm_bodies]
+puts movies[:donnie_darko]
+puts movies[:halloween]
